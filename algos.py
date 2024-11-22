e5c89e9c2dbe0a297617a3c1253fc999bd2c2337
W. Brad Meyer <wbradmeyer@gmail.com>
2024-11-22 13:35:15 -0500

    alternating cases function and test

--- algos.py
+++ algos.py
@@ -1,2 +1,11 @@
 def reverse_string(str):
     return str[::-1]
+
+def alternate_cases(str):
+    result = ''
+    for i in range(0, len(str)):
+        if i % 2:
+            result += str[i].upper()
+        else:
+            result += str[i].lower()
+    return result
--- test_algos.py
+++ test_algos.py
@@ -5,6 +5,9 @@ class TestAlgos(unittest.TestCase):
     def test_reverse_string(self):
         self.assertEqual(algos.reverse_string('hello'), 'olleh')
 
+    def test_alterate_cases(self):
+        self.assertEqual(algos.alternate_cases('hello'), 'hElLo')
+        self.assertEqual(algos.alternate_cases('sUPERcaliFrag'), 'sUpErCaLiFrAg')
 
 if __name__ == 'main':
     unittest.main()
