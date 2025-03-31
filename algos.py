ee8e5f2f2aac14f0c4c080d52266c75c5005ee67
W. Brad Meyer <wbradmeyer@gmail.com>
2025-03-31 13:03:24 -0400

    add edge of start less than 0

--- algos.py
+++ algos.py
@@ -14,7 +14,7 @@ def starts_with_a_vowel(str):
     return True if str[0] in ['a','e','i','o','u'] else False
 
 def add_alternating_indices(arr, start=0):
-    if start >= len(arr): return False
+    if start >= len(arr) or start < 0: return False
     sum = 0
     for i in range(start, len(arr), 2):
         sum += arr[i]
